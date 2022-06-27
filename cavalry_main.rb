5f162952ab30e5f5a50868cd9da1d7745ea2f3f7
Hennie Bruurmijn <hlm.bruurmijn@gmail.com>
2022-06-27 13:12:39 +0200

    Attempt at implementing building the m-way tree

--- cavalry_main.rb
+++ cavalry_main.rb
@@ -3,3 +3,5 @@
 require_relative './lib/game'
 
 ChessGame.new
+
+# For reference: https://chess.stackexchange.com/questions/34588/how-many-moves-needed-for-a-knight-to-go-from-any-square-to-any-other-square#:~:text=VI.,the%20squares%20have%20opposite%20colors)
--- lib/game.rb
+++ lib/game.rb
@@ -16,14 +16,14 @@ class ChessGame
   private
 
   def start
-    introduction
-    @board.display_board
+    # introduction
+    # @board.display_board
     @starting_point = starting_point
-    @board.update_state(@starting_point)
-    @board.display_board
+    # @board.update_state(@starting_point)
+    # @board.display_board
     @knight = Knight.new(@starting_point)
-    @ending_point = ending_point
-    # check_path
+    # @ending_point = ending_point
+    build_tree(@knight)
   end
 
   # def check_path(knight = @knight)
@@ -33,4 +33,74 @@ class ChessGame
   #   knight.generate
   #   level_order(knight)
   # end
+
+  def build_tree(node, level = 0)
+    level += 1
+    return if level > 6
+
+    generate_p1(node, level)
+  end
+
+  def generate_p1(node, level)
+    column = node.column - 1
+    row = node.row - 2
+    node.p1 = Knight.new([row, column]) if column.between?(0, 7) && row.between?(0, 7)
+    build_tree(node.p1, level) unless node.p1.nil?
+    generate_p2(node, level)
+  end
+
+  def generate_p2(node, level)
+    column = node.column - 2
+    row = node.row - 1
+    node.p2 = Knight.new([row, column]) if column.between?(0, 7) && row.between?(0, 7)
+    build_tree(node.p2, level) unless node.p2.nil?
+    generate_p3(node, level)
+  end
+
+  def generate_p3(node, level)
+    column = node.column - 2
+    row = node.row + 1
+    node.p3 = Knight.new([row, column]) if column.between?(0, 7) && row.between?(0, 7)
+    build_tree(node.p3, level) unless node.p3.nil?
+    generate_p4(node, level)
+  end
+
+  def generate_p4(node, level)
+    column = node.column - 1
+    row = node.row + 2
+    node.p4 = Knight.new([row, column]) if column.between?(0, 7) && row.between?(0, 7)
+    build_tree(node.p4, level) unless node.p4.nil?
+    generate_p5(node, level)
+  end
+
+  def generate_p5(node, level)
+    column = node.column + 1
+    row = node.row + 2
+    node.p5 = Knight.new([row, column]) if column.between?(0, 7) && row.between?(0, 7)
+    build_tree(node.p5, level) unless node.p5.nil?
+    generate_p6(node, level)
+  end
+
+  def generate_p6(node, level)
+    column = node.column + 2
+    row = node.row + 1
+    node.p6 = Knight.new([row, column]) if column.between?(0, 7) && row.between?(0, 7)
+    build_tree(node.p6, level) unless node.p6.nil?
+    generate_p7(node, level)
+  end
+
+  def generate_p7(node, level)
+    column = node.column + 2
+    row = node.row - 1
+    node.p7 = Knight.new([row, column]) if column.between?(0, 7) && row.between?(0, 7)
+    build_tree(node.p7, level) unless node.p7.nil?
+    generate_p8(node, level)
+  end
+
+  def generate_p8(node, level)
+    column = node.column + 1
+    row = node.row - 2
+    node.p8 = Knight.new([row, column]) if column.between?(0, 7) && row.between?(0, 7)
+    build_tree(node.generate_p8, level) unless node.p8.nil?
+  end
 end
