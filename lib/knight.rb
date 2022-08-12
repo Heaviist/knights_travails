0115266166053b869909b621b636fbb8b02e8eb3
Hennie Bruurmijn <hlm.bruurmijn@gmail.com>
2022-08-12 14:35:46 +0700

    Implement new approach for the knight moves
    The previous dabblings have mostly been removed. Just the interactions and board building are left.
    
    Currently trying to implement a different solution using a MOVES variable listing all the possible knight moves and then using this to generate all the possible valid squares the knight can go to. These squares will be stored as an instance variable in each Knight.
    
    From the most probably go for a BFS method, generating new Knights as needed on the go.

--- lib/knight.rb
+++ lib/knight.rb
@@ -2,18 +2,23 @@
 
 # Initialize a knight on the board. Has 8 possible moves from any position.
 class Knight
-  attr_accessor :column, :row, :p1, :p2, :p3, :p4, :p5, :p6, :p7, :p8
+  MOVES = [[-1, -2], [-2, -1], [-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2]].freeze
+
+  attr_accessor :position, :moves
 
   def initialize(position = [0, 0])
-    @column = position[1]
-    @row = position[0]
-    @p1 = nil
-    @p2 = nil
-    @p3 = nil
-    @p4 = nil
-    @p5 = nil
-    @p6 = nil
-    @p7 = nil
-    @p8 = nil
+    @position = position
+    @moves = create_moves(position)
+  end
+
+  private
+
+  def create_moves(position, possible_moves = [])
+    MOVES.each do |move|
+      column = position[0] + move[0]
+      row = position[1] + move[1]
+      possible_moves << [column, row] if column.between?(0, 7) && row.between?(0, 7)
+    end
+    possible_moves
   end
 end
